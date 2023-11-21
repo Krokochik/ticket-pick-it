08bac15e2d8d411eb9ff1a06b4b076851993911d
Krokochik <pasha09braginec@gmail.com>
2023-11-21 21:41:58 +0300

    Initial commit

---
+++ Backend/src/main/java/krokochik/backend/ServletInitializer.java
@@ -0,0 +1,14 @@
+package krokochik.backend;
+
+import krokochik.backend.controller.Controller;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BackendApplication.class);
+    }
+
+}
